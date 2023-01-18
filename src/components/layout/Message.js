2fc7625d73b3060ee02c5f3098fe2363b3d42f04
Mauricio Augusto Junior | Hospital Unimed Poços de Caldas <email@email.com>
2023-01-18 19:54:52 -0300

    Terceiro commit do dia 18/01/23

--- src/components/layout/Message.js
+++ src/components/layout/Message.js
@@ -1,6 +1,14 @@
+import { useState, useEffect } from 'react-router-dom'
+
 import styles from './Message.module.css'
 
-function Message({ type, msg }) {
-    return<div className={`${styles.message} ${styles[type]}`}>{msg}</div>>
+function Message({type, msg}) {
+
+    const [visible, setVisible] = useState(false)
+
+    return (
+        <div className={`${styles.message} ${styles[type]}`}>{msg}</div>
+    )
+}
 
 export default Message
