bfcfd1763bc01b571dc8718d9558e5cce8993eb4
Maurício Augusto Júnior <ma.juniorkf@hotmail.com>
2023-01-23 06:33:49 -0300

    Primeiro commit dia 23/01/23

--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom'
 import styles from './ProjectCard.module.css'
 
 import {BsPencil, BsFillTrashFill} from 'react-icons/bs'
@@ -9,12 +10,16 @@ function ProjectCard({id, name, budget, category, handleRemove}) {
             <p>
                 <span>Orçamento:</span> R${budget}
             </p>
-            <p>
-                <span></span> {category}
+            <p className={styles.category_text}>
+                <span className={`${styles[category.toLowerCase()]}`}></span> {category}
             </p>
-            <div>
-                <p>Editar</p>
-                <p>Remover</p>
+            <div className={styles.project_card_actions}>
+                <Link to="/">
+                    <BsPencil />Editar
+                </Link>
+                <button>
+                    <BsFillTrashFill />
+                </button>
             </div>
         </div>
     )
