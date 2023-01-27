0eb283e8b4d135dbee48a918d84d17f2feb6ac14
Maurício Augusto Júnior <ma.juniorkf@hotmail.com>
2023-01-27 07:34:53 -0300

    Primeiro commit dia 27/01/23

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,6 +3,9 @@ import styles from './Project.module.css'
 import {useParams} from 'react-router-dom'
 import {useState, useEffect} from 'react'
 
+import Loading from '../layout/Loading'
+import Container from '../layout/Container'
+
 function Project() {
 
     const {id} = useParams()
@@ -10,7 +13,8 @@ function Project() {
     const [project, setProject] = useState([])
 
     useEffect(() => {   
-        fetch(`http://localhost:5000/projects/${id}`, {
+        setTimeout(() => {
+            fetch(`http://localhost:5000/projects/${id}`, {
             method: 'GET',
             headers: {
                 'Content-type': 'application/json',
@@ -21,10 +25,25 @@ function Project() {
             setProject(data)
         })
         .catch((err) => console.log(err))
-
+        }, 300)
     }, [id])
 
-    return <p>{project.name}</p>
+    return (
+        <>
+            {project.name ? ( 
+                <div>
+                    <Container customClass="column">
+                        <div>
+                            <h1>Projeto: {project.name}</h1>
+                            <button>Editar</button>
+                        </div>
+                    </Container>
+                </div>
+            ) : (
+                <Loading />
+            )}
+        </>
+    )
 }
 
 export default Project
