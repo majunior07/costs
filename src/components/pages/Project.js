7bc1cd82ecd5258283d30ebb62f2e4fc1fdc2de5
Maurício Augusto Júnior <ma.juniorkf@hotmail.com>
2023-01-31 15:25:34 -0300

    Segundo commit dia 31/01/23

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -14,8 +14,10 @@ function Project() {
     
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
     const [type, setType] = useState()
+    
 
     useEffect(() => {   
         setTimeout(() => {
@@ -35,6 +37,8 @@ function Project() {
 
 
     function editPost(project) {
+        setMessage('')
+
         // budget validation
         if(project.budget < project.cost) {
             setMessage('O orçamento não pode ser menor que o custo do projeto!')
@@ -61,6 +65,10 @@ function Project() {
 
     function toggleProjectForm(){
         setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm)
     }
 
     return (
@@ -95,7 +103,20 @@ function Project() {
                                     />
                                 </div>
                             )}
-                        </div>                         
+                        </div>           
+                        <div className={styles.service_form_container}>
+                            <h2>Adicione um serviço:</h2>
+                            <button className={styles.btn} onClick={toggleServiceForm}>
+                                {!showServiceForm ? 'Adicionar serviço' : 'Fechar'}
+                            </button>
+                            <div className={styles.project_info}>
+                                {showServiceForm && <div>formulario do serviço</div>}
+                            </div>
+                        </div>          
+                        <h2>Serviços</h2>    
+                        <Container customClass="start">
+                            <p>Itens de serviços</p>
+                        </Container>
                     </Container>
                 </div>
             ) : (
@@ -106,6 +127,12 @@ function Project() {
 }
 
 export default Project
+
+
+
+
+
+
 
 
 
